319f0360b1e0eb012939a00ac2a4aabf1bbe4f29
AdarshAnand-cg <adarsh.anand.cg@gmail.com>
2026-08-27 17:14:33 +0530

    Update Index Page.

--- script.js
+++
@@ -1,8 +0,0 @@
-let a = 10;
-let b = 5;
-
-console.log(a + b); // Addition
-console.log(a - b); // Subtraction
-console.log(a * b); // Multiplication
-console.log(a / b); // Division
-console.log(a % b); // Remainder
